79c9149e2587fce80b73042d45c37a23c39269ee
prdy14 <118160201+prdy14@users.noreply.github.com>
2024-12-20 11:51:04 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-flow-diagram",
+  base: "/react-flow-diagram/",
   plugins: [react()],
 });
